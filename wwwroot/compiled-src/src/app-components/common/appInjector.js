27ee60b1814e6cf64f065988ec71e2e40dd62be0
IAMtheIAM <iammygod@icloud.com>
2016-10-27 13:16:42 -0700

    Remove files that shouldn't be in repo

--- wwwroot/compiled-src/src/app-components/common/appInjector.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var appInjectorRef;
-exports.appInjector = function (injector) {
-    if (injector) {
-        appInjectorRef = injector;
-    }
-    return appInjectorRef;
-};
-//# sourceMappingURL=appInjector.js.map
--- wwwroot/compiled-src/src/app-components/directives/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-// Application level directive
-__export(require('./router-active.directive'));
-//# sourceMappingURL=index.js.map
