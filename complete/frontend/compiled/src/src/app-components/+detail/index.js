de50f97bcb5adba56b272d0bd385bb3bd87a31a2
IAMtheIAM <nathanielyhvh@gmail.com>
2016-11-03 13:19:41 -0700

    Update .gitignore Remove undeed compiled typescript files

--- complete/frontend/compiled/src/src/app-components/+detail/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-// Detail Module
-__export(require('./detail.module'));
-//# sourceMappingURL=index.js.map
